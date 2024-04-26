b5e1970733c7aded2dcaaf01c0fb0b5c10bbc83e
vittorix <vittorix@gmail.com>
2024-04-26 21:07:44 +0200

    adding authors functionality. required some fixes to what's in  the video https://www.youtube.com/watch?v=esy4nRuShl8

--- routes/authors.js
+++ routes/authors.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const Author = require("../models/author");
 
 router.get("/", (req, res) => {
   res.render("authors/index");
 });
 
 router.get("/new", (req, res) => {
-  res.render("authors/new");
+  res.render("authors/new", { author: new Author() });
 });
 
 router.post("/", (req, res) => {
-  res.render("Create");
-});
+  const author = new Author({
+    name: req.body.name,
+  });
 
+  author
+    .save()
+    .then((newAuthor) => {
+      res.render("authors");
+    })
+    .catch((err) => {
+      res.render("authors/new", {
+        author: author,
+        errorMessage: "Error Creating Author: " + author.name,
+      });
+    });
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -6,12 +6,14 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
